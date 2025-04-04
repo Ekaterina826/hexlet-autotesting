9a082cc11e15fa46c18dd6f6d7bad7cdd77c6752
Ekaterina Timofeeva <ketrinkot14@gmail.com>
2025-04-04 23:15:35 +0300

    Заменила ручные утверждения

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,8 @@
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+// Проверка сменилась с отрицательной на положительную
+assert(capitalize('') === '');
+assert(capitalize('hello') === 'Hello');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
-
-console.log('Все тесты пройдены!');
+console.log("Все тесты пройдены!");
